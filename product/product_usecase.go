e60f0f68f8152911f3b5d4e379f5631a43c22e09
Thiago <thiago.dev.x@gmail.com>
2025-06-09 16:29:28 -0300

    use case

---
+++ product/product_usecase.go
@@ -0,0 +1,13 @@
+package product
+
+type ProductUseCase struct {
+	repository ProductRepository
+}
+
+func NewProductUseCase(repository ProductRepository) *ProductUseCase {
+	return &ProductUseCase{repository: repository}
+}
+
+func (uc *ProductUseCase) GetProductByID(id int) *Product {
+	return uc.repository.GetProductByID(id)
+}
